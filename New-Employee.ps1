e3744696015fd295c61e8d9b6833f37d35c83a98
KevinP <proutiere.kevin@gmail.com>
2026-01-30 16:42:13 +0100

    Ajout du script d'onboarding

--- New-Employee.ps1
+++ New-Employee.ps1
@@ -2,31 +2,46 @@ param(
     [Parameter(Mandatory=$true)][string]$Prenom,
     [Parameter(Mandatory=$true)][string]$Nom,
     [Parameter(Mandatory=$true)][string]$Titre,
-    [Parameter(Mandatory=$true)][string]$Departement,
-    [string]$Manager
+    [Parameter(Mandatory=$true)][string]$Departement
 )
 
-# 1. Génération auto des identifiants
+# 1. Génération automatique des identifiants
 $Login = ($Prenom.Substring(0,1) + $Nom).ToLower()
 $Email = "$Login@techsecure.fr"
-$Password = -join ((65..90) + (97..122) + (48..57) | Get-Random -Count 12 | % {[char]$_}) + "!"
+
+# Génération d'un mot de passe aléatoire (plus pro que "P@ssw0rd123!")
+$Password = -join ((65..90) + (97..122) + (48..57) | Get-Random -Count 10 | % {[char]$_}) + "!"
 $SecurePassword = ConvertTo-SecureString $Password -AsPlainText -Force
 
-# 2. Détermination de l'OU (Mapping)
-$OUPath = switch ($Departement) {
-    "Informatique" { "OU=Developpement,OU=Informatique,OU=Utilisateurs,OU=TechSecure,DC=formation,DC=lan" }
-    "RH"           { "OU=RH,OU=Utilisateurs,OU=TechSecure,DC=formation,DC=lan" }
-    "Commercial"   { "OU=Commercial,OU=Utilisateurs,OU=TechSecure,DC=formation,DC=lan" }
-    Default        { "OU=Utilisateurs,OU=TechSecure,DC=formation,DC=lan" }
+# 2. Mapping automatique des OU selon le département
+$BaseOU = "OU=Utilisateurs,OU=TechSecure,DC=formation,DC=lan"
+$TargetOU = switch ($Departement) {
+    "Informatique" { "OU=Developpement,OU=Informatique,$BaseOU" }
+    "RH"           { "OU=RH,$BaseOU" }
+    "Commercial"   { "OU=Commercial,$BaseOU" }
+    Default        { $BaseOU }
 }
 
 # 3. Création de l'utilisateur
-Write-Host "--- Création de $Login ---" -ForegroundColor Cyan
-New-ADUser -Name "$Prenom $Nom" -SamAccountName $Login -EmailAddress $Email -Title $Titre -Department $Departement -Path $OUPath -AccountPassword $SecurePassword -Enabled $true -ChangePasswordAtLogon $true
-
-# 4. Ajout aux groupes (Logique métier)
-Add-ADGroupMember -Identity "GRP_IT" -Members $Login
-if ($Departement -eq "Informatique") { Add-ADGroupMember -Identity "GRP_Developpeurs" -Members $Login }
-
-# 5. Simulation Email
-Write-Host "EMAIL ENVOYÉ À $Email : Bienvenue ! Votre mot de passe est $Password" -ForegroundColor Green
+try {
+    Write-Host "Création de l'utilisateur $Login..." -ForegroundColor Cyan
+    New-ADUser -Name "$Prenom $Nom" `
+               -SamAccountName $Login `
+               -UserPrincipalName "$Login@formation.lan" `
+               -EmailAddress $Email `
+               -Title $Titre `
+               -Department $Departement `
+               -Path $TargetOU `
+               -AccountPassword $SecurePassword `
+               -Enabled $true `
+               -ChangePasswordAtLogon $true
+    
+    # 4. Ajout au groupe IT par défaut
+    Add-ADGroupMember -Identity "GRP_IT" -Members $Login
+    
+    Write-Host "SUCCÈS : Utilisateur créé dans $TargetOU" -ForegroundColor Green
+    Write-Host "MOT DE PASSE TEMPORAIRE : $Password" -ForegroundColor Yellow
+}
+catch {
+    Write-Error "ERREUR : Impossible de créer l'utilisateur. $($_.Exception.Message)"
+}
